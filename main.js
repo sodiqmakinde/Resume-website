b79cf89833b511ce4d2ad37e9e8bf59d43c69a87
MicroHub69 <87417063+MicroHub69@users.noreply.github.com>
2021-09-10 14:59:10 +0100

    test nav btn

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
-function myFunction() {
-    var x = document.getElementById("navbar");
-    if (x.style.display === "none"){
-        x.style.display = "block" ;
-    } else {
-        x.style.display = "none";
+const navBar = () => {
+    const burger = document.querySelector(".burger")
+    const list = document.querySelector(".quicklink")
+    const links = document.querySelectorAll(".quicklink li")
+
+
+//toggle button
+
+burger.addEventListener("click", () => {
+    list.classList.toggle("scroll-in")
+
+    //Animation Links
+    links.forEach((link, index) =>{
+    if(link.style.animation){
+        link.style.animation = "";
+    } else{
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 9 + 0.5}s`;
     }
+   });
+
+    //bURGER aNIMATION
+    burger.classList.toggle("toggle");
+  });
 }
+
+navBar()
